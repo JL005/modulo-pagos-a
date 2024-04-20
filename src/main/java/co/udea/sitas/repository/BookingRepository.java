9265f9088ad4ed7ce23879b03e6a5d952a3c85ab
Juan Fernando Lopera Muñoz <100541364+JL005@users.noreply.github.com>
2024-04-20 11:09:33 -0500

    fix: refactor packages

--- src/main/java/co/udea/sitas/repository/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package co.udea.sitas.repository;
-
-import co.udea.sitas.model.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-}
--- src/main/java/co/udea/sitas/repository/PaymentMethodRepository.java
+++
@@ -1,9 +0,0 @@
-package co.udea.sitas.repository;
-
-import co.udea.sitas.model.paymentmethod.PaymentMethod;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Long> {
-}
